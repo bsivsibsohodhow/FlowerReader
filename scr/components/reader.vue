0b13589dc4c6a8decce5fe44a8c31d824e799c85
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-12 09:23:39 -0500

    add read function

--- scr/components/reader.vue
+++ scr/components/reader.vue
@@ -1,15 +1,34 @@
 <template>
+
   <div>
     <p>{{ flower }}</p>
   </div>
+  
 </template>
 
 <script>
+
   module.exports = {
-    data() {
-      return {
-        flower: 'default value'
+    methods: {
+      readTextFromFile(file) {
+        file.readFile('/data/issue-1.txt', function(text) {
+          var textByLine = text.split('\n');
+        })
       }
+    },
+    data() {
+     return {
+       flower: textByLine[1];
+     }
     }
   }
 </script>
+
+<style scoped>
+
+p {
+  font-family: "Futura";
+  font-size: 16px;
+}
+
+</style>
